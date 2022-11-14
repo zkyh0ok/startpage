1bc4ffa1dcd2c58a81f5e57fb8f422174afce388
Michael Neuper <73108749+michaelneuper@users.noreply.github.com>
2022-11-14 14:27:42 +0200

    Update script.js
    Now always displays hours and minutes as 2 digits

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 `use strict`;
 
-var today = new Date();
-console.log(today);
+// display time as hh:mm
+const options = {
+    timeZone:"Africa/Johannesburg",
+    hour12 : false,
+    hour:  "2-digit",
+    minute: "2-digit",
+}
 
-// format time as hh:mm
-var time = today.getHours() + ':' + today.getMinutes();
+var today = new Date();
+
+var time = today.toLocaleTimeString("en-ZA", options);
 
 document.getElementById("time").textContent = time;
